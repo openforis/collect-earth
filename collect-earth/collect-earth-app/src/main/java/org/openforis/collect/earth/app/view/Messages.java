551e0bb124eecb3a2a4d1774d1bdf83939218dae
Alfonso Sanchez-Paus Diaz <Alfonso.SanchezPausDiaz@fao.org>
2015-05-19 12:58:38 +0200

    Fixed portuguese translations http://foris.fao.org/jira/browse/EYE-83

--- collect-earth/collect-earth-app/src/main/java/org/openforis/collect/earth/app/view/Messages.java
+++ collect-earth/collect-earth-app/src/main/java/org/openforis/collect/earth/app/view/Messages.java
@@ -1,13 +1,15 @@
 package org.openforis.collect.earth.app.view;
 
+import java.util.Enumeration;
 import java.util.Locale;
 import java.util.MissingResourceException;
+import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 
 import javax.swing.JComponent;
 
 public class Messages {
-	private static final String BUNDLE_NAME = "org.openforis.collect.earth.app.view.Messages"; //$NON-NLS-1$
+	public static final String BUNDLE_NAME = "org.openforis.collect.earth.app.view.Messages"; //$NON-NLS-1$
 
 	private static ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 
@@ -26,5 +28,35 @@ public class Messages {
 		} catch (MissingResourceException e) {
 			return '!' + key + '!';
 		}
+	}
+	
+	
+	/**
+	 * Utility method to find the labels that have not yet been translated from the English original to another language
+	 * The output (in the console) are the original english lables that need to be translated and set in the destination language
+	 * @param toLanguage The language to check against english (so far it can be ES,PT or FR)
+	 */
+	private static void printMissingTranslations(String toLanguage){
+
+		PropertyResourceBundle originalEnglishLabels = (PropertyResourceBundle) ResourceBundle.getBundle(Messages.BUNDLE_NAME, Locale.ENGLISH);
+		PropertyResourceBundle translatedLabels = (PropertyResourceBundle)  ResourceBundle.getBundle(Messages.BUNDLE_NAME, new Locale(toLanguage));
+		
+		// Go through the contents of the original English labels and try to find the translation
+		// If the translation is not found then print the original to console
+		Enumeration<String> keys = originalEnglishLabels.getKeys();
+		
+		String key = null;
+		while( keys.hasMoreElements() ){
+			key = keys.nextElement();
+			String translatedValue = (String) translatedLabels.handleGetObject(key);
+			if( translatedValue == null || translatedValue.length() == 0 ){
+				System.out.println( key + "=" + originalEnglishLabels.getString(key) );
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		printMissingTranslations( args.length==0?"ES":args[0]);
 	}
 }
