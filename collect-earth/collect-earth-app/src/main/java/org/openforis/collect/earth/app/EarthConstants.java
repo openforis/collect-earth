3920dffef6217614c183a1243dca61097f1ea6dd
Alfonso Sanchez-Paus Diaz <Alfonso.SanchezPausDiaz@fao.org>
2015-09-24 16:16:37 +0200

    Added support for Vietnamese http://foris.fao.org/jira/browse/EYE-118

--- collect-earth/collect-earth-app/src/main/java/org/openforis/collect/earth/app/EarthConstants.java
+++ collect-earth/collect-earth-app/src/main/java/org/openforis/collect/earth/app/EarthConstants.java
@@ -60,7 +60,7 @@ public class EarthConstants {
 	public enum SAMPLE_SHAPE{ SQUARE_CIRCLE, SQUARE, CIRCLE, OCTAGON};
 	
 	public enum UI_LANGUAGE{ 
-		FR( "Français", new Locale("fr", "FR") ) , EN( "English", new Locale("en", "EN") ), ES( "Español", new Locale("es", "ES")), PT("Português", new Locale("pt","PT") ), LA("Lao", new Locale("la","LA") ) ; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$ //$NON-NLS-9$ //$NON-NLS-10$ //$NON-NLS-11$ //$NON-NLS-12$
+		FR( "Français", new Locale("fr", "FR") ) , EN( "English", new Locale("en", "EN") ), ES( "Español", new Locale("es", "ES")), PT("Português", new Locale("pt","PT") ), VI("tiếng Việt", new Locale("vi","VI") ) , LA("Lao", new Locale("la","LA") ) ; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$ //$NON-NLS-9$ //$NON-NLS-10$ //$NON-NLS-11$ //$NON-NLS-12$
 		
 		private Locale locale;
 		private String label;
