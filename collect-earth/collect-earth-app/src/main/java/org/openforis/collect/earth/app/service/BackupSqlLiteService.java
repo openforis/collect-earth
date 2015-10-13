db2165d5ede6956a2bd750d8052b78d07d18b66d
Alfonso Sanchez-Paus Diaz <Alfonso.SanchezPausDiaz@fao.org>
2015-10-13 14:32:25 +0200

    Updated to latest Collect

--- collect-earth/collect-earth-app/src/main/java/org/openforis/collect/earth/app/service/BackupSqlLiteService.java
+++ collect-earth/collect-earth-app/src/main/java/org/openforis/collect/earth/app/service/BackupSqlLiteService.java
@@ -10,17 +10,12 @@ import java.util.Date;
 
 import javax.annotation.PostConstruct;
 
-import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
-import net.lingala.zip4j.model.ZipParameters;
-import net.lingala.zip4j.util.Zip4jConstants;
 
 import org.apache.commons.dbcp.BasicDataSource;
 import org.openforis.collect.earth.app.CollectEarthUtils;
 import org.openforis.collect.earth.app.EarthConstants;
 import org.openforis.collect.earth.app.service.LocalPropertiesService.EarthProperty;
-import org.openforis.collect.persistence.SurveyImportException;
-import org.openforis.idm.metamodel.xml.IdmlParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -51,7 +46,7 @@ public class BackupSqlLiteService {
 	private Logger logger = LoggerFactory.getLogger( BackupSqlLiteService.class );
 
 	@PostConstruct
-	public void init() throws FileNotFoundException, IdmlParseException, SurveyImportException {
+	public void init() throws FileNotFoundException {
 		attachShutDownHook();
 	}
 
