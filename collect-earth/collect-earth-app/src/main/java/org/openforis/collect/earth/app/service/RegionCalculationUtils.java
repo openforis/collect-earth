43c9b24f0d1e9662d3eaca8e949ac017c4c11eac
herrtunante <herrtunante@users.noreply.github.com>
2015-11-19 10:01:53 +0100

    Improved handling od tree/shrub perpolot calculations

--- collect-earth/collect-earth-app/src/main/java/org/openforis/collect/earth/app/service/RegionCalculationUtils.java
+++ collect-earth/collect-earth-app/src/main/java/org/openforis/collect/earth/app/service/RegionCalculationUtils.java
@@ -5,6 +5,9 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 
@@ -35,7 +38,7 @@ public class RegionCalculationUtils {
 	private final Logger logger = LoggerFactory.getLogger(RegionCalculationUtils.class);
 
 	private static final String NO_DATA_LAND_USE = "noData"; //$NON-NLS-1$
-	
+
 	@Autowired
 	EarthSurveyService earthSurveyService;
 
@@ -44,36 +47,79 @@ public class RegionCalculationUtils {
 
 	@Autowired
 	private BasicDataSource rdbDataSource;
-	
+
 	@Autowired
 	private SchemaService schemaService;
 
 	private JdbcTemplate jdbcTemplate;
-	
-	
+
+
 	@PostConstruct
 	public void initialize() {
 		jdbcTemplate = new JdbcTemplate(rdbDataSource);
 	}
-	
+
 	public void handleRegionCalculation(){
 		try {
 			createWeightFactors();
+
 			// If the region_areas.csv is not present then try to add the areas "per attribute" using the file areas_per_attribute.csv
+			boolean areasAdded = false;
 			if(!addAreasPerRegion()){
-				addAreasPerAttribute();
+				if( addAreasPerAttribute() ){
+					areasAdded = true;
+				}
+			}else{
+				areasAdded = true;
 			}
+
+			if( areasAdded ){
+				handleNumberOfTrees();
+				handleNumberOfShrubs();
+				recalculatePlotWeights();
+			}
+
 		} catch (Exception e) {
-			logger.error( "Error when calculating hte expansion factors for the plots ", e);
+			logger.error( "Error when calculating the expansion factors for the plots ", e);
 		}
 	}
-	
+
+	private void recalculatePlotWeights() {
+		String schemaName = schemaService.getSchemaPrefix();
+		List<Double> distinctExpFactors = jdbcTemplate.queryForList("SELECT DISTINCT(" + EXPANSION_FACTOR + ") FROM " + schemaName  + "plot", Double.class);
+
+		ArrayList<Double> actualExpFactors = new ArrayList<Double>();
+		for (Object expFactor : distinctExpFactors) {
+			if( expFactor!= null ){
+				actualExpFactors.add( (Double) expFactor );
+			}
+		}
+		
+		// Sort in ascending order
+		Collections.sort(actualExpFactors);
+
+		double minimunEF = actualExpFactors.get(0);
+
+		for (double expansionFactor : actualExpFactors) {
+			// The plots with the minimum expansion factor will have a weight of 1
+			double plotWeight = expansionFactor/minimunEF;
+			final Object[] updateValues = new Object[2];
+
+			updateValues[0] = plotWeight;
+			updateValues[1] = expansionFactor;
+
+			jdbcTemplate.update("UPDATE " + schemaName + "plot SET "+PLOT_WEIGHT+"=? WHERE " + EXPANSION_FACTOR +" =?", updateValues); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		}
+
+
+	}
+
 	private void createWeightFactors(){
 		final String schemaName = schemaService.getSchemaPrefix();
 		jdbcTemplate.execute("ALTER TABLE " + schemaName + "plot ADD " + EXPANSION_FACTOR + " FLOAT"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 		jdbcTemplate.execute("ALTER TABLE " + schemaName + "plot ADD " + PLOT_WEIGHT + " FLOAT"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 	}
-	
+
 	/**
 	 * This is the "old way"of assigning an expansion factor (the area in hecaters that a plot represents) to a plot based on the information form the "region_areas.csv" file.
 	 * @return True if there was a region_areas.csv file, false if not present so that areas were not assigned.
@@ -83,7 +129,7 @@ public class RegionCalculationUtils {
 
 		final File regionAreas = new File( localPropertiesService.getProjectFolder() + File.separatorChar + REGION_AREAS_CSV);
 		String schemaName = schemaService.getSchemaPrefix();
-		
+
 		if (regionAreas.exists()) {
 
 			try {
@@ -106,32 +152,30 @@ public class RegionCalculationUtils {
 						if( plots_per_region.intValue() != 0 ){
 							expansion_factor_hectars_calc = (float)area_hectars / (float) plots_per_region.intValue();
 						}
-					
+
 						final Object[] updateValues = new Object[4];
 						updateValues[0] = expansion_factor_hectars_calc;
 						updateValues[1] = plot_weight;
 						updateValues[2] = region;
 						updateValues[3] = plot_file;
 						jdbcTemplate.update("UPDATE " + schemaName + "plot SET "+EXPANSION_FACTOR+"=?, "+PLOT_WEIGHT+"=? WHERE region=? OR plot_file=?", updateValues); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-						
+
 					} catch (NumberFormatException e) {
 						logger.error("Possibly the header", e); //$NON-NLS-1$
 					} 
 
 				}
-				
+
 				// FINALLY ASSIGN A WEIGHT OF CERO AND AN EXPANSION FACTOR OF 0 FOR THE PLOTS WITH NO_DATA
-				
+
 				final Object[] updateNoDataValues = new Object[3];
 				updateNoDataValues[0] = 0;
 				updateNoDataValues[1] = 0;
 				updateNoDataValues[2] = NO_DATA_LAND_USE;
-				
+
 				jdbcTemplate.update("UPDATE " + schemaName + "plot SET "+EXPANSION_FACTOR+"=?, "+PLOT_WEIGHT+"=? WHERE land_use_category=?", updateNoDataValues); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-				
-				handleNumberOfTrees( schemaName );
-				handleNumberOfShrubs( schemaName );
-				
+
+
 			} catch (FileNotFoundException e) {
 				logger.error("File not found?", e); //$NON-NLS-1$
 			} catch (IOException e) {
@@ -145,9 +189,10 @@ public class RegionCalculationUtils {
 		}
 
 	}
-	
-	
-	private void handleNumberOfShrubs(String schemaName) {
+
+
+	private void handleNumberOfShrubs() {
+		String schemaName = schemaService.getSchemaPrefix();
 		// This is specific to the Global Forest Survey - Drylands monitoring assessment
 		if( AnalysisSaikuService.surveyContains(SHRUB_COUNT, earthSurveyService.getCollectSurvey() ) ){
 			// First set the number of shrubs to 30 if the user assessed that there were more than 30 shrubs on the plot
@@ -158,7 +203,8 @@ public class RegionCalculationUtils {
 		}
 	}
 
-	private void handleNumberOfTrees(String schemaName) {
+	private void handleNumberOfTrees() {
+		String schemaName = schemaService.getSchemaPrefix();
 		// This is specific to the Global Forest Survey - Drylands monitoring assessment
 		if( AnalysisSaikuService.surveyContains(TREE_COUNT, earthSurveyService.getCollectSurvey() ) ){
 			// First set the number of shrubs to 30 if the user assessed that there were more than 30 shrubs on the plot
@@ -173,52 +219,56 @@ public class RegionCalculationUtils {
 
 		final File areasPerAttribute = new File( localPropertiesService.getProjectFolder() + File.separatorChar + ATTRIBUTE_AREAS_CSV);
 		String schemaName = schemaService.getSchemaPrefix();
-		
+
 		if (areasPerAttribute.exists()) {
 
 			try {
 				CSVReader csvReader = CsvReaderUtils.getCsvReader(areasPerAttribute.getAbsolutePath());
 				String[] csvLine = null;
-				
+
 				// The header (first line) should contain the names of the three columns : attribute_name,area,weight
-				
+
 				String[] columnNames = csvReader.readNext();
-				
+
 				ArrayList<String> attributeNames = new ArrayList<String>();
-				
+
 				if(columnNames.length < 3 ){
 					throw new RuntimeException("The " + areasPerAttribute.getAbsolutePath() + " file needs have this format : attribute_name1,attribute_name2,...attribute_nameN,area,weight./nAt least one attribute is necessary. This wuuld be the attribute or attributes (their name in the survey definition) that would realte the plot with its expancion factor");
 				}
-				
+
 				for( int colPosition = 0; colPosition<columnNames.length -2; colPosition++){
 					String attributeName = columnNames[colPosition];
-									
+
 					// Validate attribute name
 					if( !isAttributeInPlotEntity( attributeName ) ){
 						throw new RuntimeException("The expected format of the CSV file at " + areasPerAttribute.getAbsolutePath() + " should be attribute_name,area,weight. The name of the attribute in hte first column of your CSV '" + attributeName + "'is not a attribute under the plot entity.");
 					}
-					
+
 					attributeNames.add(attributeName);
 				}
-				
+
 				//Validate area and weight headers.
 				if( !columnNames[ columnNames.length -2 ].equalsIgnoreCase("area") || !columnNames[columnNames.length -1].equalsIgnoreCase("weight")){
 					throw new RuntimeException("The expected format of the CSV file at " + areasPerAttribute.getAbsolutePath() + " should be attribute_name,area,weight");
 				}
-				
+
+				int line = 1;
 				while( ( csvLine = csvReader.readNext() ) != null ){
-					
-					int area_hectars = Integer.parseInt( csvLine[columnNames.length -2] );
-					final Float plot_weight =  Float.parseFloat( csvLine[columnNames.length -1] );
-					
+
+					try{
+
+
+						int area_hectars = Integer.parseInt( csvLine[columnNames.length -2] );
+						final Float plot_weight =  Float.parseFloat( csvLine[columnNames.length -1] );
+
 						int numberOfAttributes = attributeNames.size();
-					
+
 						ArrayList<Object> attributeValues = new ArrayList<Object>(); 
 						for(int attributeValueCol = 0; attributeValueCol<numberOfAttributes;attributeValueCol++){
 							attributeValues.add(csvLine[attributeValueCol]);
 						}						
 
-						
+
 						StringBuffer selectQuery = new StringBuffer();
 						selectQuery.append("SELECT count(  DISTINCT ").append(EarthConstants.PLOT_ID).append(") FROM ").append(schemaName).append("plot  WHERE ");
 						for (int attr =0; attr<attributeNames.size() ; attr++) {
@@ -229,18 +279,18 @@ public class RegionCalculationUtils {
 							}else{
 								selectQuery.append("=? AND ");
 							}
-							
+
 						}
 
 						Integer plots_per_region = jdbcTemplate.queryForObject(selectQuery.toString(), attributeValues.toArray(), Integer.class); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-					
+
 						// Calculate the expansion factor. Simply the deivision of the area for the selected attributes by the amount of plots that match the attribute values
 						Float expansion_factor_hectars_calc = 0f;
 						if( plots_per_region.intValue() != 0 ){
 							expansion_factor_hectars_calc = (float)area_hectars / (float) plots_per_region.intValue();
 						}
 
-						
+
 						// Build the update query
 						StringBuffer updateQuery = new StringBuffer();
 						updateQuery.append("UPDATE ").append(schemaName).append("plot SET ").append(EXPANSION_FACTOR).append("=?, ").append(PLOT_WEIGHT).append("=? WHERE ");
@@ -252,19 +302,22 @@ public class RegionCalculationUtils {
 							}else{
 								updateQuery.append("=? AND ");
 							}
-							
+
 						}
-						
+
 						// Add the expansion factor and plot_weight to the values that will be sent with the update
 						attributeValues.add(0, expansion_factor_hectars_calc);
 						attributeValues.add(1, plot_weight);
-						
+
 						jdbcTemplate.update(updateQuery.toString(), attributeValues.toArray()); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+					}catch( Exception e5){
+						logger.error("Problem in line number " + line + " with values "  + Arrays.toString( csvLine ), e5 );
+					}finally{
+						line++;
+					}
 				}
-				
-				handleNumberOfTrees( schemaName );
-				handleNumberOfShrubs( schemaName );
-								
+
+
 			} catch (FileNotFoundException e) {
 				logger.error("File not found?", e); //$NON-NLS-1$
 			} catch (IOException e) {
