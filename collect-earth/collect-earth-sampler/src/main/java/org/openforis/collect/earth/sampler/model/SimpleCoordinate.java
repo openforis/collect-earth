ffc901baa5bb7e1168e4e1eae21087209c8e746e
herrtunante <herrtunante@users.noreply.github.com>
2016-05-02 23:21:23 +0200

    Avoid multiple loading of browser widows when working on the same plot

--- collect-earth/collect-earth-sampler/src/main/java/org/openforis/collect/earth/sampler/model/SimpleCoordinate.java
+++ collect-earth/collect-earth-sampler/src/main/java/org/openforis/collect/earth/sampler/model/SimpleCoordinate.java
@@ -61,4 +61,37 @@ public class SimpleCoordinate {
 	public String toString() {
 		return latitude + "," + longitude;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((latitude == null) ? 0 : latitude.hashCode());
+		result = prime * result
+				+ ((longitude == null) ? 0 : longitude.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SimpleCoordinate other = (SimpleCoordinate) obj;
+		if (latitude == null) {
+			if (other.latitude != null)
+				return false;
+		} else if (!latitude.equals(other.latitude))
+			return false;
+		if (longitude == null) {
+			if (other.longitude != null)
+				return false;
+		} else if (!longitude.equals(other.longitude))
+			return false;
+		return true;
+	}
 }
