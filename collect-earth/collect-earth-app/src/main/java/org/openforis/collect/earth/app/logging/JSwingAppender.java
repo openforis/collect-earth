1e963a5b659ced515c0a9cedb7ecf0d06fab9e23
Alfonso Sanchez-Paus Diaz <Alfonso.SanchezPausDiaz@fao.org>
2018-08-14 01:11:44 +0200

    Improved logging by sowing an option pane with error message

--- collect-earth/collect-earth-app/src/main/java/org/openforis/collect/earth/app/logging/JSwingAppender.java
+++ collect-earth/collect-earth-app/src/main/java/org/openforis/collect/earth/app/logging/JSwingAppender.java
@@ -1,6 +1,10 @@
 package org.openforis.collect.earth.app.logging;
 
+import java.awt.Dimension;
+
+import javax.swing.JEditorPane;
 import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
 import javax.swing.SwingUtilities;
 
 import org.apache.logging.log4j.core.Filter;
@@ -13,17 +17,15 @@ import org.apache.logging.log4j.core.config.plugins.PluginElement;
 import org.apache.logging.log4j.core.config.plugins.PluginFactory;
 import org.apache.logging.log4j.core.layout.PatternLayout;
 
-@Plugin(name = "JDialogAppender", category = "Core", elementType = "appender", printObject = true)
-public class JDialogAppender extends AbstractAppender {
+@Plugin(name = "JSwingAppender", category = "Core", elementType = "appender", printObject = true)
+public class JSwingAppender extends AbstractAppender {
 
-	private static final long serialVersionUID = 1L;
-
-	public JDialogAppender(String name, Filter filter, Layout<?> layout, boolean ignoreExceptions) {
+	public JSwingAppender(String name, Filter filter, Layout<?> layout, boolean ignoreExceptions) {
 		super(name, filter, layout, ignoreExceptions);
 	}
 
 	@PluginFactory
-	public static JDialogAppender createAppender(@PluginAttribute("name") String name,
+	public static JSwingAppender createAppender(@PluginAttribute("name") String name,
 			@PluginElement("Layout") Layout<?> layout, @PluginElement("Filters") Filter filter,
 			@PluginAttribute("ignoreExceptions") boolean ignoreExceptions) {
 
@@ -35,7 +37,7 @@ public class JDialogAppender extends AbstractAppender {
 		if (layout == null) {
 			layout = PatternLayout.createDefaultLayout();
 		}
-		return new JDialogAppender(name, filter, layout, ignoreExceptions);
+		return new JSwingAppender(name, filter, layout, ignoreExceptions);
 	}
 
 	@Override
@@ -48,12 +50,19 @@ public class JDialogAppender extends AbstractAppender {
 			SwingUtilities.invokeLater(new Runnable() {
 				@Override
 				public void run() {
-					JTextArea = 
-					JOptionPane joptionPane = new JOptionPane(message, messageType, optionType)
+					JEditorPane web = new JEditorPane();
+					web.setEditable(false);
+					web.setContentType("text/html");
+					web.setText(message);
+
+					JScrollPane scrollPane = new JScrollPane(web);
+					scrollPane.setPreferredSize(new Dimension(450, 350));
+
+					JOptionPane.showMessageDialog(null, scrollPane, "Error has been loogged", JOptionPane.ERROR_MESSAGE);
 				}
 			});
 		} catch (final IllegalStateException e) {
-			// ignore case when the platform hasn't yet been iniitialized
+			// ignore case when the platform hasn't yet been initialized
 		}
 	}
 }
