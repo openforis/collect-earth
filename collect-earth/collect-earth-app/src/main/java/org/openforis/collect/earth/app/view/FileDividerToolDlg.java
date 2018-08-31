e88e6dae32a8d510b60fa7f8c31f2a3ac983431d
Alfonso Sanchez-Paus Diaz <sanchez.paus@gmail.com>
2018-08-31 12:10:36 +0200

    Change the limit of the number of CSV files that can be generated to 500

--- collect-earth/collect-earth-app/src/main/java/org/openforis/collect/earth/app/view/FileDividerToolDlg.java
+++ collect-earth/collect-earth-app/src/main/java/org/openforis/collect/earth/app/view/FileDividerToolDlg.java
@@ -38,7 +38,7 @@ import org.slf4j.LoggerFactory;
 public class FileDividerToolDlg extends JDialog{
 
 	private static final long serialVersionUID = 2241706750062961024L;
-	private static final int MAX_FILES = 50;
+	private static final int MAX_FILES = 500;
 	private CollectSurvey survey;
 	private Logger logger = LoggerFactory.getLogger( FileDividerToolDlg.class); 
 	private JComboBox<Integer> numberOfFiles ;
