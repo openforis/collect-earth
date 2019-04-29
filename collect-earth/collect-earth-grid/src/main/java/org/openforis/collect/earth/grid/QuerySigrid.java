07b4ad5cbe4babcb04b3b63c17c733fee0898e29
Alfonso Sanchez-Paus Diaz <Alfonso.SanchezPausDiaz@fao.org>
2019-04-29 09:51:23 +0200

    more queries

--- collect-earth/collect-earth-grid/src/main/java/org/openforis/collect/earth/grid/QuerySigrid.java
+++ collect-earth/collect-earth-grid/src/main/java/org/openforis/collect/earth/grid/QuerySigrid.java
@@ -21,7 +21,7 @@ public class QuerySigrid {
 		return database.getPlots(grid, boundingBox[0], boundingBox[1], boundingBox[2], boundingBox[3], gridDistance);
 	}
 
-
+	// Bounding box array : Double maxX, Double maxY, Double minX, Double minY
 	public void writeCsvFromBoundingBox( Double[] boundingBox, Integer gridDistance, Integer grid, String prefix  ) {
 
 
@@ -50,8 +50,8 @@ public class QuerySigrid {
 		//querySigrid.writeCsvFromBoundingBox( new Double[] {38d,38d,-17d,14d}, 1000, 10, "NorthAfrica");
 		//querySigrid.writeCsvFromBoundingBox( new Double[] {60d,38d,-17d,-35d}, 1000, 10, "AllAfricaAfrica");
 		//querySigrid.writeCsvFromBoundingBox( new Double[] {11.7d, 37.5d, 7.2d, 30d}, 1000, 1, "Tunisia_1000");
-		querySigrid.writeCsvFromBoundingBox( new Double[] {6.034, 14.862, 1.687, 12.725}, 1000, 1, "FFEM_1000");
-		
+		//querySigrid.writeCsvFromBoundingBox( new Double[] {6.034, 14.862, 1.687, 12.725}, 1000, 1, "FFEM_1000");
+		querySigrid.writeCsvFromBoundingBox( new Double[] {34.434541, -7.869966, 21.344595, -18.697429}, 1000, 1, "Zambia");
 		System.exit(0);
 	}
 }
