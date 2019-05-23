153d9b1fb0375b39d78cfdd7b9c2a8d747aa6c29
Alfonso Sanchez-Paus Diaz <Alfonso.SanchezPausDiaz@fao.org>
2019-05-23 15:26:55 +0200

    Show progress monitor when updating DB through CSV

--- collect-earth/collect-earth-app/src/main/java/org/openforis/collect/earth/app/view/ImportActionListener.java
+++ collect-earth/collect-earth-app/src/main/java/org/openforis/collect/earth/app/view/ImportActionListener.java
@@ -25,7 +25,7 @@ public final class ImportActionListener implements ActionListener {
 	protected static final Integer YES_TO_ALL = 1;
 	protected static final Integer NO = 2;
 	protected static final Integer NO_TO_ALL = 3;
-	
+
 	private final DataFormat importFormat;
 	private JFrame frame;
 	private LocalPropertiesService localPropertiesService;
@@ -56,14 +56,14 @@ public final class ImportActionListener implements ActionListener {
 				+"<br/>" //$NON-NLS-1$
 				+ Messages.getString("ImportActionListener.2") //$NON-NLS-1$
 				+ "</html>";
-		
+
 		if( !moreThanOneFiles ){
-			
+
 			final int selectedOption = JOptionPane.showConfirmDialog(null,
 					message  //$NON-NLS-1$
 					,Messages.getString("ImportActionListener.3"),  //$NON-NLS-1$
 					JOptionPane.YES_NO_OPTION);
-	
+
 			if (selectedOption == JOptionPane.YES_OPTION){
 				return YES;
 			}else if (selectedOption == JOptionPane.NO_OPTION){
@@ -72,12 +72,12 @@ public final class ImportActionListener implements ActionListener {
 				return JOptionPane.CLOSED_OPTION;
 			}
 		}else{
-			
-			 String[] buttons = { Messages.getString("YES"), Messages.getString("YES_TO_ALL"), Messages.getString("NO"), Messages.getString("NO_TO_ALL") };
 
-			 return JOptionPane.showOptionDialog(null, message, Messages.getString("ImportActionListener.3") ,
-			        JOptionPane.WARNING_MESSAGE, 0, null, buttons, buttons[1]);
-	
+			String[] buttons = { Messages.getString("YES"), Messages.getString("YES_TO_ALL"), Messages.getString("NO"), Messages.getString("NO_TO_ALL") };
+
+			return JOptionPane.showOptionDialog(null, message, Messages.getString("ImportActionListener.3") ,
+					JOptionPane.WARNING_MESSAGE, 0, null, buttons, buttons[1]);
+
 		}
 	}
 
@@ -86,68 +86,73 @@ public final class ImportActionListener implements ActionListener {
 		final ImportXMLDialogProcessMonitor importDialogProcessMonitor = new ImportXMLDialogProcessMonitor();
 		if (filesToImport != null) {
 
-			
+
 			switch (importType) {
 			case ZIP_WITH_XML:
-					new Thread("XML Import Thread " ){ //$NON-NLS-1$
-						@Override
-						public void run() {
-							Integer importNonFinishedPlots = shouldImportNonFinishedRecords( filesToImport.length > 1);
-							boolean firstFile = true;
-							
-							for (final File importedFile : filesToImport) {								
-								XMLDataImportProcess dataImportProcess = null;
-								try{
-									if ( firstFile ){
-										firstFile = false;
-									}else if ( !firstFile && (importNonFinishedPlots == YES || importNonFinishedPlots == NO  ) ){
-										importNonFinishedPlots = shouldImportNonFinishedRecords( filesToImport.length > 1);
-									}
-	
-									if( importNonFinishedPlots.equals( JOptionPane.CLOSED_OPTION ) ){
-										break;
-									}
-									
-									boolean importNotFinished= importNonFinishedPlots.equals( YES ) || importNonFinishedPlots.equals( YES_TO_ALL );
-																		
-									dataImportProcess = dataImportService.getImportSummary(importedFile, importNotFinished);
-									importDialogProcessMonitor.startImport(dataImportProcess, frame, dataImportService, importedFile );
-									
-								} catch (Exception e1) {
-									logger.error("Error importing data" , e1); //$NON-NLS-1$
-									importDialogProcessMonitor.closeProgressmonitor();
-									JOptionPane.showMessageDialog( frame,  importedFile.getName() + " - " + Messages.getString("CollectEarthWindow.3"), importedFile.getName() + " - " + Messages.getString("CollectEarthWindow.7"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-											JOptionPane.ERROR_MESSAGE);
-									logger.error("Error importing data from " + importedFile.getAbsolutePath() + " in format " + importType.name() , e1); //$NON-NLS-1$ //$NON-NLS-2$
-								} finally {
-									IOUtils.closeQuietly(dataImportProcess);
+				new Thread("XML Import Thread " ){ //$NON-NLS-1$
+					@Override
+					public void run() {
+						Integer importNonFinishedPlots = shouldImportNonFinishedRecords( filesToImport.length > 1);
+						boolean firstFile = true;
+
+						for (final File importedFile : filesToImport) {								
+							XMLDataImportProcess dataImportProcess = null;
+							try{
+								if ( firstFile ){
+									firstFile = false;
+								}else if ( !firstFile && (importNonFinishedPlots == YES || importNonFinishedPlots == NO  ) ){
+									importNonFinishedPlots = shouldImportNonFinishedRecords( filesToImport.length > 1);
 								}
+
+								if( importNonFinishedPlots.equals( JOptionPane.CLOSED_OPTION ) ){
+									break;
+								}
+
+								boolean importNotFinished= importNonFinishedPlots.equals( YES ) || importNonFinishedPlots.equals( YES_TO_ALL );
+
+								dataImportProcess = dataImportService.getImportSummary(importedFile, importNotFinished);
+								importDialogProcessMonitor.startImport(dataImportProcess, frame, dataImportService, importedFile );
+
+							} catch (Exception e1) {
+								logger.error("Error importing data" , e1); //$NON-NLS-1$
+								importDialogProcessMonitor.closeProgressmonitor();
+								JOptionPane.showMessageDialog( frame,  importedFile.getName() + " - " + Messages.getString("CollectEarthWindow.3"), importedFile.getName() + " - " + Messages.getString("CollectEarthWindow.7"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+										JOptionPane.ERROR_MESSAGE);
+								logger.error("Error importing data from " + importedFile.getAbsolutePath() + " in format " + importType.name() , e1); //$NON-NLS-1$ //$NON-NLS-2$
+							} finally {
+								IOUtils.closeQuietly(dataImportProcess);
 							}
 						}
-					}.start();
+					}
+				}.start();
 				break;
 			case CSV:
-				for (final File importedFile : filesToImport) {
+				new Thread("Update using CSV Thread " ){ //$NON-NLS-1$
+					@Override
+					public void run() {
+						for (final File importedFile : filesToImport) {
 
-					CSVDataImportProcess importSurveyAsCsv = null;
-					try {
-						importSurveyAsCsv = dataImportService.getCsvImporterProcess(importedFile);
+							CSVDataImportProcess importSurveyAsCsv = null;
+							try {
+								importSurveyAsCsv = dataImportService.getCsvImporterProcess(importedFile);
 
-						if( importSurveyAsCsv != null ){
-							importSurveyAsCsv.init();
-							ProcessStatus status = importSurveyAsCsv.getStatus();
-							status.setTotal( getTotalNumberOfLines( importedFile ) );
-							if ( status != null && ! importSurveyAsCsv.getStatus().isError() ) {
-								ImportProcessMonitorDialog importProcessWorker = new ImportProcessMonitorDialog(importSurveyAsCsv, frame );
-								importProcessWorker.start();
+								if( importSurveyAsCsv != null ){
+									importSurveyAsCsv.init();
+									ProcessStatus status = importSurveyAsCsv.getStatus();
+									status.setTotal( getTotalNumberOfLines( importedFile ) );
+									if ( status != null && ! importSurveyAsCsv.getStatus().isError() ) {
+										ImportProcessMonitorDialog importProcessWorker = new ImportProcessMonitorDialog(importSurveyAsCsv, frame );
+										importProcessWorker.start();
+									}
+								}
+							} catch (Exception e1) {
+								JOptionPane.showMessageDialog(frame, Messages.getString("CollectEarthWindow.7") + "\n" + e1.getMessage(), importedFile.getName() + " - " + Messages.getString("CollectEarthWindow.3"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+										JOptionPane.ERROR_MESSAGE);
+								logger.error("Error importing data from " + importedFile.getAbsolutePath() + " in format " + importType , e1); //$NON-NLS-1$ //$NON-NLS-2$
 							}
 						}
-					} catch (Exception e1) {
-						JOptionPane.showMessageDialog(this.frame, Messages.getString("CollectEarthWindow.7") + "\n" + e1.getMessage(), importedFile.getName() + " - " + Messages.getString("CollectEarthWindow.3"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-								JOptionPane.ERROR_MESSAGE);
-						logger.error("Error importing data from " + importedFile.getAbsolutePath() + " in format " + importType , e1); //$NON-NLS-1$ //$NON-NLS-2$
 					}
-				}
+				}.start();
 				break;
 			case FUSION:
 				break;
@@ -162,9 +167,9 @@ public final class ImportActionListener implements ActionListener {
 		BufferedReader br = null;
 		try {
 			br = new BufferedReader(new InputStreamReader(new FileInputStream(importedFile)));
-	
+
 			while ( br.readLine() != null)   {
-			  count++;
+				count++;
 			}
 		} catch (IOException e) {
 			logger.error("Error counting the number of lines in file " + importedFile.getAbsolutePath() , e) ; //$NON-NLS-1$
@@ -176,8 +181,8 @@ public final class ImportActionListener implements ActionListener {
 					logger.error("Error closing file reader " + importedFile.getAbsolutePath() , e) ;
 				}
 			}
-			
-			
+
+
 		}
 		return count;
 	}
--- collect-earth/collect-earth-app/src/main/java/org/openforis/collect/earth/app/view/ImportProcessMonitorDialog.java
+++ collect-earth/collect-earth-app/src/main/java/org/openforis/collect/earth/app/view/ImportProcessMonitorDialog.java
@@ -1,8 +1,11 @@
 package org.openforis.collect.earth.app.view;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.ProgressMonitor;
+import javax.swing.SwingUtilities;
 
+import org.openforis.collect.earth.app.CollectEarthUtils;
 import org.openforis.collect.io.ReferenceDataImportStatus;
 import org.openforis.collect.io.metadata.parsing.ParsingError;
 import org.openforis.collect.manager.process.AbstractProcess;
@@ -10,12 +13,17 @@ import org.openforis.collect.utils.ExecutorServiceUtil;
 
 public class ImportProcessMonitorDialog extends ProcessMonitorDialog<Void, ReferenceDataImportStatus<ParsingError>> {
 
+	private JFrame parentFrame;
+
 
 	public ImportProcessMonitorDialog(AbstractProcess<Void, ReferenceDataImportStatus<ParsingError>> importProcess, JFrame parentFrame ) {
 		super();
 		this.process = importProcess;
-		progressMonitor = new ProgressMonitor(parentFrame, Messages.getString("ExportDialogProcessMonitor.0"), Messages.getString("ExportDialogProcessMonitor.1"), 0, 100); //$NON-NLS-1$ //$NON-NLS-2$
-		progressMonitor.setMillisToPopup(1000);
+		this.parentFrame = parentFrame;
+		SwingUtilities.invokeLater( () -> {
+				progressMonitor = new ProgressMonitor(parentFrame, Messages.getString("ExportDialogProcessMonitor.0"), Messages.getString("ExportDialogProcessMonitor.1"), 0, 100); //$NON-NLS-1$ //$NON-NLS-2$
+				progressMonitor.setMillisToPopup(1000);
+		} );
 	}
 
 	protected String getProcessActionMessage() {
@@ -27,8 +35,11 @@ public class ImportProcessMonitorDialog extends ProcessMonitorDialog<Void, Refer
 	public void run() {
 
 		try {
-			ExecutorServiceUtil.executeInCachedPool(process);
-			
+			monitorProgress();
+			process.call();
+			if( process.getStatus().isComplete() ) {
+				JOptionPane.showMessageDialog( parentFrame, "Update finished", "CSV Update", JOptionPane.INFORMATION_MESSAGE);
+			}
 		} catch (final Exception e) {
 			logger.error("Error starting the process", e); //$NON-NLS-1$
 		}
