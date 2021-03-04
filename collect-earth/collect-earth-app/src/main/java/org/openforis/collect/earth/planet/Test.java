041d7ff6232fa60249d7b50843ce387f2b8e606c
Alfonso Sanchez-Paus Diaz <Alfonso.SanchezPausDiaz@fao.org>
2021-03-04 15:00:35 +0100

    added test for available monmths

--- collect-earth/collect-earth-app/src/main/java/org/openforis/collect/earth/planet/Test.java
+++ collect-earth/collect-earth-app/src/main/java/org/openforis/collect/earth/planet/Test.java
@@ -8,11 +8,11 @@ public class Test {
 
 	public static void main(String[] args) {
 		try {
-			PlanetImagery planet = new PlanetImagery( "8dfb61e5458c4d3ab595dc24e160b55b" );
+			PlanetImagery planet = new PlanetImagery( "4c4414d961524d07b5ea01f03c95b21a" );
 			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-			Date start = formatter.parse("2019-11-01");
-			Date end =  formatter.parse("2019-12-01");
-			String[] itemTypes = {"PSScene3Band", "PSScene4Band"};/*
+			Date start = formatter.parse("2013-01-01");
+			Date end =  formatter.parse("2013-12-01");
+			String[] itemTypes = {"PSScene3Band", "PSScene4Band", "REOrthoTile"};/*
 			double[][][] coords = {{
 				{
 					-1.8230438232421875,
@@ -35,11 +35,15 @@ public class Test {
 					5.66433079911972
 				}  }
 			};
-			
-			double[][][] coords = {{{-1.457541, 114.592016}, {-1.457541, 114.592896}, {-1.458427, 114.592896}, {-1.458427, 114.592016}, {-1.457541, 114.592016}}};*/
-			double[][][] coords = {{{-39.611883, -72.612975}, {-39.611883, -72.611833}, {-39.612765, -72.611833}, {-39.612765, -72.612975}, {-39.611883, -72.612975}}};
-			System.out.println( 
-					planet.getLayerUrl(start, end, coords, itemTypes ) 
+			*/
+
+			double[][][] coords = {{{148.440013,-5.763688}, {148.440645,-5.763688}, {148.440645,-5.76432}, {148.440013,-5.76432}, {148.440013,-5.763688}}};
+			//double[][][] coords = {{{-39.611883, -72.612975}, {-39.611883, -72.611833}, {-39.612765, -72.611833}, {-39.612765, -72.612975}, {-39.611883, -72.612975}}};
+			System.out.println(
+					planet.getAvailableDates(start, end, coords, itemTypes )
+			);
+			System.out.println(
+					planet.getLayerUrl(start, end, coords, itemTypes )
 			);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
