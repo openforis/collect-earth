224d24ed74f87d7f61c14eb5eafd174403d07ca6
Alfonso Sanchez-Paus Diaz <Alfonso.SanchezPausDiaz@fao.org>
2021-05-26 15:08:10 +0200

    Organized imports

--- collect-earth/collect-earth-sampler/src/main/java/org/openforis/collect/earth/sampler/adhoc/UruguayDisplacedPoints.java
+++ collect-earth/collect-earth-sampler/src/main/java/org/openforis/collect/earth/sampler/adhoc/UruguayDisplacedPoints.java
@@ -2,7 +2,6 @@ package org.openforis.collect.earth.sampler.adhoc;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
