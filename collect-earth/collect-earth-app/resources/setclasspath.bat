1ae6768f65626fb7cea229d77bb88a6eb7c38027
herrtunante <Alfonso.SanchezPausDiaz@fao.org>
2022-11-07 11:26:59 +0100

    Fix issues starting Saiku

--- collect-earth/collect-earth-app/resources/setclasspath.bat
+++ collect-earth/collect-earth-app/resources/setclasspath.bat
@@ -6,6 +6,6 @@ rem ---------------------------------------------------------------------------
 
 echo SET ABSOLUTE REFERENCE TO COLLECT EARTH JRE
 rem Sets the location of the Collect Earth provided JRE. This is done in a post-installation action from the installer which replaces C:\OpenForis\CollectEarth/java with the actual path where Collect Earth is installed
-set _RUNJAVA="%JAVA_HOME%\bin\java"
+set _RUNJAVA="%_JAVA_HOME%\bin\java"
 
 exit /b 0
