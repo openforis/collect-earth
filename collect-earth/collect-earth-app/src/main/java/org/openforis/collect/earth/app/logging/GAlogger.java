ecbeca08dfd7a67373e3fee75fbb14166eb3959e
Alfonso Sanchez-Paus Diaz <Alfonso.SanchezPausDiaz@fao.org>
2023-03-07 23:34:02 +0100

    Close Http connection

--- collect-earth/collect-earth-app/src/main/java/org/openforis/collect/earth/app/logging/GAlogger.java
+++ collect-earth/collect-earth-app/src/main/java/org/openforis/collect/earth/app/logging/GAlogger.java
@@ -12,6 +12,7 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.slf4j.Logger;
@@ -26,9 +27,8 @@ public class GAlogger {
 
 			@Override
 			public void run() {
-				try {
+				try( CloseableHttpClient httpclient = HttpClients.createDefault() ) {
 					// Following instruction from https://developers.google.com/analytics/devguides/collection/protocol/ga4/sending-events?hl=en&client_type=gtag
-					HttpClient httpclient = HttpClients.createDefault();
 					HttpPost httppost = new HttpPost("https://www.google-analytics.com/mp/collect");
 					
 					// Request parameters and other properties.
