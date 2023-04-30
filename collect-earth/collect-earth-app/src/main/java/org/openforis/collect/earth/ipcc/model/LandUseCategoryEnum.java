4b47db3e834bfd3a2c71cf4aa095a25448a37683
herrtunante <Alfonso.SanchezPausDiaz@fao.org>
2023-04-30 22:06:33 +0200

    Correct Settlemet-Grassland ids

--- collect-earth/collect-earth-app/src/main/java/org/openforis/collect/earth/ipcc/model/LandUseCategoryEnum.java
+++ collect-earth/collect-earth-app/src/main/java/org/openforis/collect/earth/ipcc/model/LandUseCategoryEnum.java
@@ -3,9 +3,9 @@ package org.openforis.collect.earth.ipcc.model;
 public enum LandUseCategoryEnum {
 	F ("F", "Forest", 1), 
 	C ("C", "Cropland", 2), 
-	S ("S", "Settlement", 3), 
+	G ("G","Grassland", 3), 
 	W ("W", "Wetland", 4), 
-	G ("G","Grassland", 5), 
+	S ("S", "Settlement", 5), 
 	O ("O", "Otherland", 6);
 	
 	private final String code;
