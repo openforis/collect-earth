7f21ff07853e56fc194b55db393b89ed444c3062
herrtunante <Alfonso.SanchezPausDiaz@fao.org>
2023-06-28 14:48:42 +0200

    send as json

--- collect-earth/collect-earth-app/src/main/java/org/openforis/collect/earth/app/logging/GAlogger.java
+++ collect-earth/collect-earth-app/src/main/java/org/openforis/collect/earth/app/logging/GAlogger.java
@@ -2,25 +2,23 @@ package org.openforis.collect.earth.app.logging;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class GAlogger {
 
 	private static final Logger logger = LoggerFactory.getLogger(GAlogger.class);
-
+	private static final String API_SECRET = "E11gVKqxSamFWwCswJPKIQ";
+	private static final String MEASUREMENT_ID = "G-8K943HZKJZ";
+	
 	public static void logGAnalytics(String event) {
 		new Thread("GA logging thread") {
 
@@ -28,21 +26,24 @@ public class GAlogger {
 			public void run() {
 				try( CloseableHttpClient httpclient = HttpClients.createDefault() ) {
 					// Following instruction from https://developers.google.com/analytics/devguides/collection/protocol/ga4/sending-events?hl=en&client_type=gtag
-					HttpPost httppost = new HttpPost("https://www.google-analytics.com/mp/collect");
-					
+					HttpPost httppost = new HttpPost("https://www.google-analytics.com/mp/collect?api_secret="+API_SECRET+"&measurement_id="+ MEASUREMENT_ID);
+					//  See https://ga-dev-tools.google/ga4/event-builder/?p=2&d=0&f=1&c=custom_event&j=PlotSaved&n=CollectEarth&k=E11gVKqxSamFWwCswJPKIQ&i=G-8K943HZKJZ&e=1687947214291000&m=W10&b=W1tdXQ
 					// Request parameters and other properties.
-					List<NameValuePair> params = new ArrayList<>(4);
-					params.add(new BasicNameValuePair("api_secret", "E11gVKqxSamFWwCswJPKIQ"));
-					params.add(new BasicNameValuePair("measurement_id", "G-8K943HZKJZ"));
-					params.add(new BasicNameValuePair("client_id", "CollectEarth"));
-					params.add(new BasicNameValuePair(
-							"events", 
-							"[{\"name\": \"" + event +"\",\"params\": {\"engagement_time_msec\": \"100\",\"session_id\": \""  + ( new Date() ).getTime() + "\"} }]"
-							)
-					);
-					httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
-					httppost.addHeader("content-type", "application/x-www-form-urlencoded");
+					
+					String jsonToSend = "{"
+							+ "  \"client_id\": \"CollectEarth\","
+							+ "  \"timestamp_micros\": \"" + (new Date() ).getTime() + "\","
+							+ "  \"non_personalized_ads\": true,"
+							+ "  \"events\": ["
+							+ "    {"
+							+ "      \"name\": \"" + event + "\"\r\n"
+							+ "    }"
+							+ "  ]"
+							+ "}";
 
+					httppost.addHeader("content-type", "application/json");
+					httppost.setEntity( new StringEntity(jsonToSend) );
+				    
 					//Execute and get the response.
 					HttpResponse response = httpclient.execute(httppost);
 					HttpEntity entity = response.getEntity();
