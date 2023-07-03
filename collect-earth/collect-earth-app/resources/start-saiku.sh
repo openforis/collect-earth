95faca9c7359f5c956c766eafab0f21110fc3861
herrtunante <Alfonso.SanchezPausDiaz@fao.org>
2023-07-03 15:06:50 +0200

    Add the Linux and MacOS start saiku script

---
+++ collect-earth/collect-earth-app/resources/start-saiku.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+DIR_REL=`dirname $0`
+cd $DIR_REL
+DIR=`pwd`
+cd -
+
+. "$DIR/set-java.sh"
+
+ setJava
+
+ cd "$DIR/tomcat/bin"
+ export CATALINA_OPTS="-Xms256m -Xmx768m -Dfile.encoding=UTF-8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -Djava.awt.headless=true"
+ # JAVA_HOME=$_JAVA_HOME
+ sh startup.sh
+
